df487fb040a186e22d6dff3f6a3d66b9353e0dfc
Jackie Jin <liveaeon@gmail.com>
2013-05-10 20:20:24 -0700

    package clened

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "downloading ardrone_autonomy package......"
+git clone git://github.com/tum-vision/ardrone_autonomy.git
+echo "building ardrone_autonomy package.......ARSDroneLib and collatorals"
+cd ardrone_autonomy && ./build_sdk.sh && cd ..
 rosmake ardrone_autonomy
+echo "downloading the tum package......"
+git clone git://github.com/tum-vision/tum_ardrone.git
+echo "rosmaking the tum package......."
 rosmake tum_ardrone
