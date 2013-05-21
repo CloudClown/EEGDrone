90a871c1126cb728844c4e02d93b86556d9d3865
Jackie Jin <liveaeon@gmail.com>
2013-05-21 16:12:25 -0700

    launch file update

--- eeg_control/src/python-mindwave/mindwaveControl.py
+++ eeg_control/src/python-mindwave/mindwaveControl.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #Author: Jackie Jin
 #This python interface is based on Mr.Andreas Klostermann's interface
 #with PyEEG and Pygame
@@ -10,3 +11,6 @@ from pyeeg import bin_power
 from parser import Parser
 import rospy
 
+class MindWaveControl():
+    def __init__(self):
+        
