680aa908222a4dc6667160ee9039306f70400478
Jackie Jin <liveaeon@gmail.com>
2013-05-21 15:35:02 -0700

    ros control node added

---
+++ eeg_control/src/python-mindwave/control.py
@@ -0,0 +1,12 @@
+#Author: Jackie Jin
+#This python interface is based on Mr.Andreas Klostermann's interface
+#with PyEEG and Pygame
+#This is the project link:https://github.com/akloster/python-mindwave
+#License: MIT
+
+from numpy import *
+import scipy
+from pyeeg import bin_power
+from parser import Parser
+import rospy
+
