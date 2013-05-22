5f0bd0d5d5f41bec93bccd5e6efdc5f261ada098
Jackie Jin <liveaeon@gmail.com>
2013-05-21 20:46:06 -0700

    minor fix on msg

--- eeg_control/src/python-mindwave/mindwaveControl.py
+++ eeg_control/src/python-mindwave/mindwaveControl.py
@@ -10,15 +10,33 @@ import scipy
 from pyeeg import bin_power
 from parser import Parser
 import rospy
+import sys
+import roslib; roslib.load_manifest('EEGControlMsgs')
+from EEGControlMsgs.msgs import *
 
-class MindWaveControl():
+class Mindwave():
     def __init__(self):
         rospy.init_node('MindwaveControl')
         rospy.loginfo("Initializing Control")
-    def run(self):
+        self.eegParser = Parser()
+        self.controlPublisher = rospy.Publisher('eeg/control', MindwaveControl)
+        self.controlOutput = MindwaveControl()
+        self.outputMap = {
+            'attention':0,
+            'meditation':0
+            }
+        
+    def control(self):
         pass
+
+    def publish(self):
+        self.controlOutput.attention = outputMap['attention']
+        self.controlOutput.meditation = outputMap['meditation']
+        self.controlPublisher.publish(self.controlOutput)
 
 if __name__ == '__main__':
     try:
-        control = MindWaveControl()
-        control.run()
+        control = Mindwave()
+        control.control()
+    except rospy.ROSInterruptException:
+        pass
