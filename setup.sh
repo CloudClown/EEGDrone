63ec4c74b1be31f855cfeb1761a5f6fb10cd227c
Jackie Jin <liveaeon@gmail.com>
2013-11-01 18:20:18 -0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-echo "downloading ardrone_autonomy package......"
-git clone git://github.com/tum-vision/ardrone_autonomy.git
-echo "building ardrone_autonomy package.......ARSDroneLib and collatorals"
-cd ardrone_autonomy && ./build_sdk.sh && cd ..
 rosmake ardrone_autonomy
-echo "downloading the tum package......"
-git clone git://github.com/tum-vision/tum_ardrone.git
-echo "rosmaking the tum package......."
+
+if [ $? -eq 0 ]; then
+    echo "[package] ardrone_autonomy is already installed..."
+else
+    echo "[download] ardrone_autonomy package......"
+    git clone git://github.com/tum-vision/ardrone_autonomy.git
+    echo "[build] ardrone_autonomy package.......ARSDroneLib and collatorals"
+    cd ardrone_autonomy && ./build_sdk.sh && cd ..
+    rosmake ardrone_autonomy
+fi
+
 rosmake tum_ardrone
-rosmake eeg_control
+
+if [ $? -eq 0 ]; then
+    echo "[package] tum_ardrone is already installed..."
+else
+    echo "[download] the tum package......"
+    git clone git://github.com/tum-vision/tum_ardrone.git
+    echo "[rosmake] the tum package......."
+    rosmake tum_ardrone
+    rosmake eeg_control
+fi
+
+echo "[rosmake] EEGControlMsgs..."
 rosmake EEGControlMsgs
